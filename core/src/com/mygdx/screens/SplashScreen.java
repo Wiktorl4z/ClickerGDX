99a3efd6d5541926e8f7900c8c7bedd5b401c265
Wiktor <wiktorkalinowski@gmail.com>
2016-12-20 17:21:40 +0100

    Gradle updated

--- core/src/com/mygdx/screens/SplashScreen.java
+++ core/src/com/mygdx/screens/SplashScreen.java
@@ -1,21 +1,31 @@
 package com.mygdx.screens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
 import com.mygdx.game.MyGdxGame;
+
 
 public class SplashScreen extends AbstractScreen {
 
     private Texture splashImg;
 
-    public SplashScreen(MyGdxGame game) {
+    public SplashScreen(final MyGdxGame game) {
         super(game);
         init();
+
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                game.setScreen(new GameplayScreen(game));
+            }
+        }, 1);
     }
 
     private void init() {
         // TODO implement better assests loading when game grows
         splashImg = new Texture("badlogic.jpg");
     }
+
 
     @Override
     public void render(float delta) {
