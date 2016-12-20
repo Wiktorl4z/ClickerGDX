be4b5cac63b4e7cac50ab367ca519cc939f60a5e
Wiktor <wiktorkalinowski@gmail.com>
2016-12-20 18:30:33 +0100

    Displaying Player Image

--- core/src/com/mygdx/screens/GameplayScreen.java
+++ core/src/com/mygdx/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package com.mygdx.screens;
 
+import com.mygdx.entities.Player;
 import com.mygdx.game.MyGdxGame;
 
 public class GameplayScreen extends AbstractScreen {
+
+    private Player player;
 
     public GameplayScreen(MyGdxGame game) {
         super(game);
@@ -10,5 +13,26 @@ public class GameplayScreen extends AbstractScreen {
     }
 
     private void init() {
+       initPlayer();
+    }
+
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
+
     }
 }
