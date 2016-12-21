752565941aa4f117c196175c7c3f2a2b1f6c3cfd
Wiktor <wiktorkalinowski@gmail.com>
2016-12-21 23:22:55 +0100

    add playerButton and player listener

--- core/src/com/mygdx/screens/GameplayScreen.java
+++ core/src/com/mygdx/screens/GameplayScreen.java
@@ -1,11 +1,15 @@
 package com.mygdx.screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.entities.Player;
 import com.mygdx.game.MyGdxGame;
 
 public class GameplayScreen extends AbstractScreen {
 
     private Player player;
+    private Button playerButton;
 
     public GameplayScreen(MyGdxGame game) {
         super(game);
@@ -13,6 +17,28 @@ public class GameplayScreen extends AbstractScreen {
 
     protected void init() {
        initPlayer();
+        initPlayerButton();
+    }
+
+    private void initPlayerButton() {
+        playerButton = new Button(new Button.ButtonStyle()); // chodzi o to aby button był przezroczysty po to dodajemy
+                                                            // Button.ButtonStyle
+        playerButton.setWidth(460);
+        playerButton.setHeight(360);
+        playerButton.setX(10);
+        playerButton.setY(170);
+        playerButton.setDebug(true);
+
+        stage.addActor(playerButton); // dodawanie Buttona do stage
+        playerButton.addListener(new ClickListener() {          // przechwytywanie akcji w Buttonie
+
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) { 
+
+                System.out.println("click");
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
     }
 
     private void initPlayer() {
