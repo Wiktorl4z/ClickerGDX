2ee3262ec2e570c65cf4bd4990e66e07d74bab57
Wiktor <wiktorkalinowski@gmail.com>
2016-12-28 16:55:26 +0100

    randomize flying efect

--- core/src/com/mygdx/entities/FlyingObject.java
+++ core/src/com/mygdx/entities/FlyingObject.java
@@ -1,6 +1,7 @@
 package com.mygdx.entities;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
@@ -16,11 +17,14 @@ public class FlyingObject extends Image {
 
     public final static String MONEY = "money.png";
     public final static String PASSIVE = "books.png";
-    private final static int WIDHT = 150;
-    private final static int HEIGHT = 150;
 
-    private final static int STARTING_X = 0;
+    private final static int WIDHT = 70;
+    private final static int HEIGHT = 70;
+
+    private final static int STARTING_X_1 = 0;
+    private final static int STARTING_X_2 = MyGdxGame.WIDTH;
     private final static int STARTING_Y = -100; // outside the window
+    private int startingX;
 
     private FlyingObjectType type;
     private MyGdxGame game;
@@ -35,7 +39,8 @@ public class FlyingObject extends Image {
         this.setSize(WIDHT, HEIGHT);
 
         // starting position
-        this.setPosition(STARTING_X, STARTING_Y);
+        startingX = MathUtils.randomBoolean() ? STARTING_X_1 : STARTING_X_2; // if else
+        this.setPosition(startingX, STARTING_Y);
 
         this.addListener(new ClickListener() {
             @Override
@@ -68,21 +73,34 @@ public class FlyingObject extends Image {
     }
 
     public void fly() {
+
+        int xSign = 0;
+        if (startingX == STARTING_X_1) {
+            xSign = 1;
+        } else {
+            xSign = -1;
+        }
+
+        int time1 = MathUtils.random(1, 6);
+        int time2 = MathUtils.random(1, 6);
+        int randomYEffect = MathUtils.random(-100, 500);
+
         Action a = Actions.parallel(
-                Actions.moveBy(300, 200, 5),
-                Actions.rotateBy(360, 5)
+                Actions.moveBy(xSign * 300 + (MathUtils.random(-200, 200)), 200 + randomYEffect, time1),
+                Actions.rotateBy(360, time1)
         );
         Action b = Actions.parallel(
-                Actions.moveBy(-500, 900, 3),
-                Actions.rotateBy(360, 5)
+                Actions.moveBy(xSign * -500 + (MathUtils.random(-200, 200)), 900, time2),
+                Actions.rotateBy(360, time2)
         );
         Action c = Actions.run(new Runnable() {
             @Override
             public void run() {
-                FlyingObject.this.remove(); // removing object, when action done
+                FlyingObject.this.remove(); // remov ing object, when action done
             }
         });
 
         this.addAction(Actions.sequence(a, b, c));
     }
 }
+
